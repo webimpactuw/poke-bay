bb921a4b17adca1b9ce70c1ece0d590aa8d5194a
celestinebuendia <celestinembuendia@yahoo.com>
2024-02-27 18:38:55 -0800

    Styled Navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,11 +1,16 @@
+import logo from "./Images/poke-bay.png";
+
 const Navbar = () => {
     return (
-    <div className = "fixed"> 
-        <ol>
-            <li><a href="/">home</a></li>
-            <li><a href="/menu">menu</a></li>
-            <li><a href="/about">about</a></li>
+    <div className = "mx-auto max-w-7xl px-2 sm:px-6 lg:px-8"> 
+    <div className = "relative flex h-16 items-center justify-between">
+        <img src={logo} alt="Poke Bay Logo" className="size-14 p-1"/>
+        <ol className="flex space-x-3">
+            <li className="text-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><a href="/">Home</a></li>
+            <li className="text-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><a href="/menu">Menu</a></li>
+            <li className="text-black hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"><a href="/about">About</a></li>
         </ol>
+        </div>
     </div>
     )
   }
