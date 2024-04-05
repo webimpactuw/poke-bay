73570b0553efa6d801e4e61a8c9d2cc2831814e5
celestinebuendia <celestinembuendia@yahoo.com>
2024-04-04 18:56:47 -0700

    Added primary colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      'inter': ['Inter'],
+      'lato': ['Lato'],
+      'oswald': ['Oswald'],
+    },
+    extend: {
+      colors: {
+        primary: '#35728C',
+        secondary: '#97C7DC',
+      },
+    },
   },
   plugins: [],
 };
