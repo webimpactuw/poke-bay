bada387d33f80702a7a08fc6f3cc15a5c261f65c
celestinebuendia <celestinembuendia@yahoo.com>
2024-04-05 20:50:08 -0700

    Removed Inter and made default font Lato

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     fontFamily: {
-      'inter': ['Inter'],
       'lato': ['Lato'],
       'oswald': ['Oswald'],
     },
