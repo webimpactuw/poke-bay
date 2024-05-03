216e2ed44f8c2c2223fc6a326e0a81e48e264236
annawng <annawang125@gmail.com>
2024-05-02 19:13:38 -0700

    Work on mobile homepage layout

--- src/HomePage/Home.tsx
+++ src/HomePage/Home.tsx
@@ -25,90 +25,91 @@ const Home = () => {
     },
   ];
   return (
-    <div className='flex flex-row'>
-      <div>
-        <div className='flex flex-col justify-center pt-[5%] pb-[280px]'>
-          <div
-            className='flex space-x-6 flex-row justify-center items-center text-[12vw] font-black font-inter'
-            aria-hidden
-          >
-            <p className='text-orange-600'>P</p>
-            <img className='w-[12vw] h-[12vw]' src={logo}></img>
-            <p className='text-green-700'>K</p>
-            <p className='text-cyan-600 pr-10'>E</p>
-            <p className='text-sky-600'>B</p>
-            <p className='text-violet-500'>A</p>
-            <p className='text-red-600'>Y</p>
-          </div>
-          <h1 className='hidden'>Poke Bay</h1>
-          <div className='flex flex-col align-center justify-center text-center'>
-            <div className='text-primary text-3xl font-light font-league'>
-              Fresh Poke in the U District
-            </div>
+    <div>
+      <div className='flex flex-col justify-center pt-32 md:pt-20 pb-[320px] md:pb-[280px]'>
+        <div
+          className='flex space-x-1 md:space-x-6 flex-row justify-center items-center text-[16vw] sm:text-[12vw] font-semibold sm:font-black font-inter'
+          aria-hidden
+        >
+          <span className='text-orange-600'>P</span>
+          <img className='w-[12vw] h-[12vw]' src={logo}></img>
+          <span className='text-green-700'>K</span>
+          <span className='text-cyan-600 pr-6 sm:pr-10'>E</span>
+          <span className='text-sky-600'>B</span>
+          <span className='text-violet-500'>A</span>
+          <span className='text-red-600'>Y</span>
+        </div>
+        <h1 className='hidden'>Poke Bay</h1>
+        <div className='flex flex-col align-center justify-center text-center'>
+          <div className='text-primary text-xl md:text-3xl font-light font-league'>
+            Fresh Poke in the U District
           </div>
         </div>
+      </div>
 
-        <div className='bg-primary home-bg mb-[320px]'>
-          <div className='text-center text-white w-5/6 m-auto flex flex-col items-center gap-16'>
-            <h2 className='font-league font-semibold text-5xl pt-5'>
-              Featured Bowls
-            </h2>
-            <div className='flex flex-col md:flex-row justify-center items-center md:justify-between'>
-              {arrayData.map((item) => (
-                <div
-                  key={item.name}
-                  className='flex flex-col gap-2 md:w-[30%] w-1/2'
-                >
-                  <img className='' src={item.image}></img>
-                  <h3 className='font-league text-2xl pt-4 font-semibold'>
-                    {item.name}
-                  </h3>
-                  <p className='font-light text-md'>{item.desc}</p>
-                </div>
-              ))}
-            </div>
-            <div className='py-3 px-6 w-fit rounded-full text-primary bg-white'>
-              <Link to='/menu'>View Full Menu</Link>
-            </div>
-          </div>
-        </div>
-
-        <div className='w-5/6 m-auto pb-24'>
-          <div className=' text-primary flex items-center max-md:flex-col justify-between'>
-            <img className='md:w-2/5' src={entrance}></img>
-            <div className='flex flex-col gap-4 max-md:items-center max-md:pt-[4%] md:w-2/5'>
-              <h2 className='font-league text-5xl font-semibold'>
-                Our Mission
-              </h2>
-              <p className='max-md:w-5/6 leading-relaxed'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </p>
-              <button className='mt-4 py-3 px-6 w-fit rounded-full bg-primary text-white'>
-                <Link to='/about'>View More</Link>
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className='w-5/6  m-auto'>
-          <div className='flex max-md:flex-col flex-row text-primary justify-between items-center'>
-            <div className='flex flex-col gap-4 md:w-1/2'>
-              <h2 className='font-league text-5xl font-semibold'>Visit Us</h2>
-              <div>
-                <p>Poke Bay</p>
-                <p>4215 University Way NE</p>
-                <p>Seattle, WA 98105</p>
+      <div className='bg-primary home-bg mb-[220px] md:mb-[320px]'>
+        <div className='text-center text-white w-5/6 m-auto flex flex-col items-center gap-4 md:gap-16'>
+          <h2 className='font-league font-semibold text-3xl md:text-5xl pt-5'>
+            Featured Bowls
+          </h2>
+          <div className='flex flex-col md:flex-row justify-center items-center md:justify-between gap-12'>
+            {arrayData.map((item) => (
+              <div key={item.name} className='flex flex-col gap-2 md:w-[30%]'>
+                <img
+                  className='sm:w-3/4 md:w-auto m-auto'
+                  src={item.image}
+                  alt=''
+                ></img>
+                <h3 className='font-league text-xl md:text-2xl pt-2 md:pt-4 font-semibold'>
+                  {item.name}
+                </h3>
+                <p className='font-light md:text-md'>{item.desc}</p>
               </div>
-              <p>Open every day from 11 AM to 9 PM</p>
-            </div>
-            <iframe
-              className='md:w-2/5 border-none aspect-[3/2]'
-              src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2687.317730479997!2d-122.31600312358195!3d47.658823684369665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54901595ef185da9%3A0x10610fc1d56cf1f3!2sPoke%20Bay!5e0!3m2!1sen!2sus!4v1712895811651!5m2!1sen!2sus'
-              loading='lazy'
-            ></iframe>
+            ))}
           </div>
+          <div className='mt-10 md:mt-0 py-3 px-6 w-fit rounded-full text-primary bg-white'>
+            <Link to='/menu'>View Full Menu</Link>
+          </div>
+        </div>
+      </div>
+
+      <div className='w-5/6 m-auto pb-24'>
+        <div className=' text-primary flex items-center max-md:flex-col max-md:gap-12 justify-between'>
+          <img className='md:w-2/5' src={entrance}></img>
+          <div className='flex flex-col gap-4 max-md:items-center md:w-2/5'>
+            <h2 className='font-league text-3xl md:text-5xl font-semibold'>
+              Our Mission
+            </h2>
+            <p className='max-md:text-center leading-relaxed'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <button className='mt-4 py-3 px-6 w-fit rounded-full bg-primary text-white'>
+              <Link to='/about'>View More</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='w-5/6 m-auto max-lg:pb-12'>
+        <div className='flex max-md:flex-col-reverse max-md:gap-12 text-primary justify-between items-center'>
+          <div className='flex flex-col gap-4 md:w-1/2 max-md:text-center'>
+            <h2 className='font-league text-3xl md:text-5xl font-semibold'>
+              Visit Us
+            </h2>
+            <div>
+              <p>Poke Bay</p>
+              <p>4215 University Way NE</p>
+              <p>Seattle, WA 98105</p>
+            </div>
+            <p>Open every day from 11 AM to 9 PM</p>
+          </div>
+          <iframe
+            className='w-full md:w-2/5 border-none aspect-[3/2]'
+            src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2687.317730479997!2d-122.31600312358195!3d47.658823684369665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54901595ef185da9%3A0x10610fc1d56cf1f3!2sPoke%20Bay!5e0!3m2!1sen!2sus!4v1712895811651!5m2!1sen!2sus'
+            loading='lazy'
+          ></iframe>
         </div>
       </div>
     </div>
