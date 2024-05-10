320662246b0c416b5ae10bc2890a8c65a511132f
annawng <annawang125@gmail.com>
2024-05-09 17:33:45 -0700

    Change to HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import About from './AboutPage/About.tsx';
 import Menu from './MenuPage/Menu.tsx';
 import Home from './HomePage/Home.tsx';
 import Root from './Root.tsx';
 import Error from './Error.tsx';
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
     {
       path: '/',
