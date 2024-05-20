43b49336d823da7b587e6ba24245dfdb9061835e
annawng <annawang125@gmail.com>
2024-05-20 16:58:50 -0700

    Return to top of screen on refresh

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,8 +1,16 @@
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Root = () => {
+  useEffect(() => {
+    // Scroll to top of screen on refresh
+    window.onbeforeunload = function () {
+      window.scrollTo(0, 0);
+    };
+  });
+
   return (
     <>
       <Navbar></Navbar>
